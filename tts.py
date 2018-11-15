ea6e6f912778fda6184bad53c54b448af2f44197
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2018-11-15 19:02:44 +0530

    Added Distance Calculator, Transact Method, Barcode Scanner, Geolocator

--- tts.py
+++ tts.py
@@ -3,10 +3,14 @@ import sys
 import json
 import time
 import pymysql as sql
+import geocoder
+import subprocess
+# from detect_barcode import VideoStream
+from math import sin, cos, sqrt, atan2, radians
 
 # Employee login
 def login(cursor):
-    rvalue = True
+    print("\n--- Employee Login ---\n")
     eid = input("\nEnter Employee ID :\t")
     while(len(eid)!=4):
         eid = input("Enter Valid Employee ID :\t")
@@ -24,12 +28,12 @@ def login(cursor):
         dbpwd = cursor.fetchone()
         if pwd == dbpwd[0]:
             print("Login Successful\n")
+            routesetup(cursor)
     else:
         reg = input("Employee ID has not been registered before. Would you like register now? (y/n)\t")
         if reg.lower() in ['y','yes']:
             register(cursor)
-    return rvalue
-
+    
 # Registration
 def register(cursor):
     print("\n--- Registration ---\n")
@@ -54,8 +58,45 @@ def register(cursor):
             if ch.lower() in ['y','yes']:
                 register(cursor)
 
+# Calculating Distance in Kms between 2 Lat,Long Pairs
+def distcalc(la1,lo1,la2,lo2):
+    R=6373.0
+    dlo = radians(lo2) - radians(lo1)
+    dla = radians(la2) - radians(la1)
+    a = sin(dla / 2)**2 + cos(la1) * cos(la2) * sin(dlo / 2)**2
+    b = 2 * atan2(sqrt(a), sqrt(1 - a))
+    res = R * b
+    return res
+
+# Setting Up Route
+def routesetup(cursor):
+    print("\n--- Route Setup ---\n")
+    routeid = input("Enter Route ID:\t")
+    sqlquery = "SELECT routeid FROM ttsdb.routes"
+    li = []
+    db_routeid_list = []
+    cursor.execute(sqlquery)
+    li = cursor.fetchall()
+    for i in li:
+        db_routeid_list.append(i[0])
+    print(db_routeid_list)
+    if routeid in db_routeid_list:
+        print("\nRoute Successfully initialized\n")
+        transact(cursor)
+    else:
+        print("Invalid Route ID")
+        routesetup(cursor)
+
+def transact(cursor):
+    while True:
+        print("\n\n-------- Welcome to Transport Ticketing System! --------\n")
+        print("Please scan your BMTC Card by placing it infront of the Camera to continue\n")
+        subprocess.check_output("python detect_barcode.py", shell=True).decode()
+        
 if __name__ == "__main__":
     print("\n\n-------- Welcome to Transport Ticketing System! --------\n")
+    g = geocoder.ip('me')
+    print(g.lat)
 
     choice = input("\n1. Login\n2. Registration\n3. Exit\n\n")
     # SQL Parameters
